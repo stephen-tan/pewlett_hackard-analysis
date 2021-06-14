ea03c1a062f9ef283f16e43fdc14bf8ad12a6a8a
Stephen Tan <82525229+stephen-tan@users.noreply.github.com>
2021-06-13 21:42:10 -0700

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -43,3 +43,47 @@ select * from retiring_titles;
 
 -- DELIVERABLE 2
 
+select * from mentorship_eligibility;
+
+create table mentorship_eligibility as
+select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date, t.title
+from employees as e
+join dept_emp as de
+on (de.emp_no = e.emp_no)
+join titles as t
+on (t.emp_no = de.emp_no)
+where (birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no;
+
+select * from dept_emp;
+
+CREATE TABLE dept_emp (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
+);
+
+-- DELIVERABLE 3
+
+create table qualified_mentorship as
+select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date, t.title
+from employees as e
+join dept_emp as de
+on (de.emp_no = e.emp_no)
+join titles as t
+on (t.emp_no = de.emp_no)
+where (birth_date between '1965-01-01' and '1965-12-31')
+and (title = 'Senior Engineer' or title = 'Staff' or title = 'Senior Staff' or title = 'Technique Leader')
+order by e.emp_no;
+
+select * from qualified_mentorship;
+
+drop table qualified_mentorship;
+
+select count(emp_no) from qualified_mentorship;
